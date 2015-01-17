413992044fe9f77de8235d54be78306f82137327
Joey Morris <rjmorris12@gmail.com>
2015-01-16 22:22:12 -0500

    Stopped re-reading the input file on each window resize.

--- js/barchart.js
+++ js/barchart.js
@@ -1,83 +1,23 @@
 var picks;
+var players;
 var num_games;
 var num_players;
 var rows;
 var cols;
+
+var tip_offset_y = 2;
+var col_pad = 4;
+var row_pad = 10;
+var name_offset_y = -15;
+var bottom_margin = 14;
 
 var sort_method = "confidence";
 var color_scheme = "dark";
 
 
 $(function() {
-    resize();
-    redraw();
-
-    $(window).resize(function() {
-        resize();
-        redraw();
-    });
-});
-
-
-function resize() {
-    var w = $(window);
-    var b = $("body");
-    b.height(w.height()
-             - parseInt(b.css("margin-top"))
-             - parseInt(b.css("margin-bottom"))
-             - parseInt(b.css("padding-top"))
-             - parseInt(b.css("padding-bottom"))
-            );
-
-    var h = $("#header");
-
-    var g = $("#graphic");
-    g.attr("width", b.width());
-    g.attr("height", b.height() - h.outerHeight(true));
-}
-
-
-function redraw() {
-    $("#graphic").empty();
-    $(".d3-tip").remove();
-
-    var width = $("#graphic").innerWidth();
-    var height = $("#graphic").innerHeight();
-
-    var tip_offset_y = 2;
-    var col_pad = 4;
-    var row_pad = 10;
-    var name_offset_y = -15;
-    var bottom_margin = 14;
-
+    set_layout_sizes();
     set_color_scheme();
-    
-    var svg = d3.select("#graphic");
-
-    var tip = d3.tip()
-        .attr('class', 'd3-tip')
-        .offset(function(d) {
-            var tw = parseInt(d3.select(".d3-tip").style("width"));
-            var cx = parseInt(d3.select(this).attr("x")) + parseInt(d3.select(this).attr("width")) / 2;
-            var sw = parseInt(d3.select("#graphic").attr("width"));
-
-            var offset_x = 0;
-            if (tw/2 > cx) offset_x = tw/2 - cx;
-            else if (cx + tw/2 > sw) offset_x = sw - (cx + tw/2);
-
-            return [tip_offset_y, offset_x];
-        })
-        .direction("s")
-        .html(function(d) {
-            var pick_class = "pick_future";
-            if (d.result == true) pick_class = "pick_right";
-            else if (d.result == false) pick_class = "pick_wrong";
-            var matchup = d.MATCHUP.replace("Semifinal winners", "Semifinal winners: " + d.pick);
-            matchup = matchup.replace(d.pick, "<span class=\"" + pick_class + "\">" + d.pick + "</span>");
-            return d.name + " [" + d.confidence + "]: " + matchup + ", " + d.game_time;
-        });
-
-    svg.call(tip);
 
     d3.tsv("picks.tsv", function(pick_data) {
         picks = pick_data;
@@ -103,152 +43,15 @@ function redraw() {
 
         num_games = picks.reduce(function(running, pick) { return Math.max(running, pick.confidence); }, 0);
 
-        var players = Object.keys(players_map).map(function(p) { return players_map[p]; });
+        players = Object.keys(players_map).map(function(p) { return players_map[p]; });
         num_players = players.length;
 
-        // Compute the positions for each row and column. There will be one row
-        // per player and one column per game. They will maximize the available
-        // space, with the exception of a little extra space at the bottom to
-        // fit the tooltip on the bottom row.
+        draw_graphic();
+    });
 
-        var row_height = (height - bottom_margin - (num_players - 1) * row_pad) / num_players;
-        var col_width = (width - (num_games - 1) * col_pad) / num_games;
-
-        rows = [];
-        rows.push({});  // dummy element to support 1-based indexing later
-        d3.range(num_players).forEach(function(i) {
-            var row = {};
-            row.height = row_height;
-            row.top = i * (row_height + row_pad);
-            row.bottom = row.top + row.height;
-            row.middle = (row.top + row.bottom) / 2;
-            rows.push(row);
-        });
-
-        cols = [];
-        cols.push({});  // dummy element to support 1-based indexing later
-        d3.range(num_games).forEach(function(i) {
-            var col = {};
-            col.width = col_width;
-            col.left = i * (col_width + col_pad);
-            col.right = col.left + col.width;
-            col.middle = (col.left + col.right) / 2;
-            cols.push(col);
-        });
-
-        // Assign the initial bar dimensions for each pick.
-
-        picks.forEach(function(pick) {
-            if (sort_method == "game") {
-                assign_bar_dimensions(pick, pick.rank, pick.game_order);
-            }
-            else {
-                assign_bar_dimensions(pick, pick.rank, pick.confidence);
-            }
-        });
-
-        
-        var bars = svg.selectAll(".bar")
-            .data(picks)
-            .enter()
-            .append("rect")
-            .classed("bar", true)
-            .attr("x", function(d) { return d.bar_left; })
-            .attr("y", function(d) { return rows[d.rank].middle; })
-            .attr("width", function(d) { return d.bar_width; })
-            .attr("height", 0)
-            .attr("rx", 4)
-            .attr("ry", 4)
-            .classed("right", function(d) { return d.result === true; })
-            .classed("wrong", function(d) { return d.result === false; })
-            .classed("unplayed", function(d) { return d.result === null; })
-            .on('mouseover.1',
-                // tip.show doesn't work when it's called inside a function.
-                // Therefore, it needs to be added as a separate event handler. d3
-                // lets you assign multiple handlers to an event by adding an
-                // extension to the event name. Here, we've added .1, .2 to the
-                // mouseover event name.
-                tip.show
-            )
-            .on('mouseover.2', function() {
-                var bar_data = d3.select(this).data()[0];
-                d3.selectAll(".highlight")
-                    .attr("r", function(d) {
-                        if (bar_data.pick == d.pick) return 3;
-                        return 4;
-                    })
-                    .classed("matching", function(d) {
-                        return (bar_data.MATCHUP == d.MATCHUP
-                                && bar_data.pick == d.pick
-                                && bar_data.name != d.name
-                               );
-                    })
-                    .classed("nonmatching", function(d) {
-                        return (bar_data.MATCHUP == d.MATCHUP
-                                && bar_data.pick != d.pick
-                                && bar_data.name != d.name
-                               );
-                    })
-                    ;
-            })
-            .on('mouseout.1',
-                // See the comments for tip.show in mouseover.1
-                tip.hide
-            )
-            .on('mouseout.2', function() {
-                d3.selectAll(".highlight")
-                    .classed("matching", false)
-                    .classed("nonmatching", false)
-                ;
-            })
-            .transition()
-            .delay(function(d) { return 1000 * (d.rank - 1) / num_players + 25 * (sort_method == "game" ? d.game_order : d.confidence); })
-            .duration(function(d) { return 250; })
-            .attr("y", function(d) { return d.bar_top; })
-            .attr("height", function(d) { return d.bar_height; })
-            ;
-
-        var highlights = svg.selectAll(".highlight")
-            .data(picks)
-            .enter()
-            .append("circle")
-            .classed("highlight", true)
-            .attr("cx", function(d) { return d.bar_hcenter; })
-            .attr("cy", function(d) { return d.bar_bottom + 4; })
-            ;
-
-        var names = svg.selectAll(".name")
-            .data(players)
-            .enter()
-            .append("g")
-            .classed("name_group", true)
-            .append("text")
-            .classed("name", true)
-            .attr("x", 0)
-            .attr("y", function(d) { return rows[d.rank].middle + name_offset_y; })
-            // dominant-baseline=hanging puts the top of the text at the y-coord.
-            //.attr("dominant-baseline", "hanging")
-            .text(function(d) { return d.name + ": " + d.score; })
-            ;
-
-        // Place a rectangle behind the names to give them some contrast when
-        // they overlap bars. This must be defined after the names, because it
-        // uses the names variable. However, the background rect element must be
-        // defined before the the name text element in the svg, because
-        // otherwise the background would be displayed on top of the name. Use
-        // the d3.insert function to insert the background rect elements before
-        // the name text elements. For the d3.insert function to work, we need
-        // the name and its background to be defined within a group (which is
-        // probably a good idea anyway).
-
-        var name_bgs = svg.selectAll(".name_group")
-            .insert("rect", ".name")
-            .classed("name_bg", true)
-            .attr("x", function(d, i) { return names[0][i].getBBox().x - 2; })
-            .attr("y", function(d, i) { return names[0][i].getBBox().y - 2; })
-            .attr("width", function(d, i) { return names[0][i].getBBox().width + 4; })
-            .attr("height", function(d, i) { return names[0][i].getBBox().height + 4; })
-            ;
+    $(window).resize(function() {
+        set_layout_sizes();
+        draw_graphic();
     });
 
     $("#sort-confidence").click(function() {
@@ -284,6 +87,203 @@ function redraw() {
         color_scheme = "dark";
         set_color_scheme("dark");
     });
+});
+
+
+function set_layout_sizes() {
+    var w = $(window);
+    var b = $("body");
+    b.height(w.height()
+             - parseInt(b.css("margin-top"))
+             - parseInt(b.css("margin-bottom"))
+             - parseInt(b.css("padding-top"))
+             - parseInt(b.css("padding-bottom"))
+            );
+
+    var h = $("#header");
+
+    var g = $("#graphic");
+    g.attr("width", b.width());
+    g.attr("height", b.height() - h.outerHeight(true));
+}
+
+
+function draw_graphic() {
+    $("#graphic").empty();
+    $(".d3-tip").remove();
+
+    var width = $("#graphic").innerWidth();
+    var height = $("#graphic").innerHeight();
+
+    // Compute the positions for each row and column. There will be one row
+    // per player and one column per game. They will maximize the available
+    // space, with the exception of a little extra space at the bottom to
+    // fit the tooltip on the bottom row.
+
+    var row_height = (height - bottom_margin - (num_players - 1) * row_pad) / num_players;
+    var col_width = (width - (num_games - 1) * col_pad) / num_games;
+
+    rows = [];
+    rows.push({});  // dummy element to support 1-based indexing later
+    d3.range(num_players).forEach(function(i) {
+        var row = {};
+        row.height = row_height;
+        row.top = i * (row_height + row_pad);
+        row.bottom = row.top + row.height;
+        row.middle = (row.top + row.bottom) / 2;
+        rows.push(row);
+    });
+
+    cols = [];
+    cols.push({});  // dummy element to support 1-based indexing later
+    d3.range(num_games).forEach(function(i) {
+        var col = {};
+        col.width = col_width;
+        col.left = i * (col_width + col_pad);
+        col.right = col.left + col.width;
+        col.middle = (col.left + col.right) / 2;
+        cols.push(col);
+    });
+
+    // Assign the bar dimensions for each pick.
+
+    picks.forEach(function(pick) {
+        if (sort_method == "game") {
+            assign_bar_dimensions(pick, pick.rank, pick.game_order);
+        }
+        else {
+            assign_bar_dimensions(pick, pick.rank, pick.confidence);
+        }
+    });
+
+    var svg = d3.select("#graphic");
+
+    var tip = d3.tip()
+        .attr('class', 'd3-tip')
+        .offset(function(d) {
+            var tw = parseInt(d3.select(".d3-tip").style("width"));
+            var cx = parseInt(d3.select(this).attr("x")) + parseInt(d3.select(this).attr("width")) / 2;
+            var sw = parseInt(d3.select("#graphic").attr("width"));
+
+            var offset_x = 0;
+            if (tw/2 > cx) offset_x = tw/2 - cx;
+            else if (cx + tw/2 > sw) offset_x = sw - (cx + tw/2);
+
+            return [tip_offset_y, offset_x];
+        })
+        .direction("s")
+        .html(function(d) {
+            var pick_class = "pick_future";
+            if (d.result == true) pick_class = "pick_right";
+            else if (d.result == false) pick_class = "pick_wrong";
+            var matchup = d.MATCHUP.replace("Semifinal winners", "Semifinal winners: " + d.pick);
+            matchup = matchup.replace(d.pick, "<span class=\"" + pick_class + "\">" + d.pick + "</span>");
+            return d.name + " [" + d.confidence + "]: " + matchup + ", " + d.game_time;
+        });
+
+    svg.call(tip);
+
+    var bars = svg.selectAll(".bar")
+        .data(picks)
+        .enter()
+        .append("rect")
+        .classed("bar", true)
+        .attr("x", function(d) { return d.bar_left; })
+        .attr("y", function(d) { return rows[d.rank].middle; })
+        .attr("width", function(d) { return d.bar_width; })
+        .attr("height", 0)
+        .attr("rx", 4)
+        .attr("ry", 4)
+        .classed("right", function(d) { return d.result === true; })
+        .classed("wrong", function(d) { return d.result === false; })
+        .classed("unplayed", function(d) { return d.result === null; })
+        .on('mouseover.1',
+            // tip.show doesn't work when it's called inside a function.
+            // Therefore, it needs to be added as a separate event handler. d3
+            // lets you assign multiple handlers to an event by adding an
+            // extension to the event name. Here, we've added .1, .2 to the
+            // mouseover event name.
+            tip.show
+           )
+        .on('mouseover.2', function() {
+            var bar_data = d3.select(this).data()[0];
+            d3.selectAll(".highlight")
+                .attr("r", function(d) {
+                    if (bar_data.pick == d.pick) return 3;
+                    return 4;
+                })
+                .classed("matching", function(d) {
+                    return (bar_data.MATCHUP == d.MATCHUP
+                            && bar_data.pick == d.pick
+                            && bar_data.name != d.name
+                           );
+                })
+                .classed("nonmatching", function(d) {
+                    return (bar_data.MATCHUP == d.MATCHUP
+                            && bar_data.pick != d.pick
+                            && bar_data.name != d.name
+                           );
+                })
+            ;
+        })
+        .on('mouseout.1',
+            // See the comments for tip.show in mouseover.1
+            tip.hide
+           )
+        .on('mouseout.2', function() {
+            d3.selectAll(".highlight")
+                .classed("matching", false)
+                .classed("nonmatching", false)
+            ;
+        })
+        .transition()
+        .delay(function(d) { return 1000 * (d.rank - 1) / num_players + 25 * (sort_method == "game" ? d.game_order : d.confidence); })
+        .duration(function(d) { return 250; })
+        .attr("y", function(d) { return d.bar_top; })
+        .attr("height", function(d) { return d.bar_height; })
+    ;
+
+    var highlights = svg.selectAll(".highlight")
+        .data(picks)
+        .enter()
+        .append("circle")
+        .classed("highlight", true)
+        .attr("cx", function(d) { return d.bar_hcenter; })
+        .attr("cy", function(d) { return d.bar_bottom + 4; })
+    ;
+
+    var names = svg.selectAll(".name")
+        .data(players)
+        .enter()
+        .append("g")
+        .classed("name_group", true)
+        .append("text")
+        .classed("name", true)
+        .attr("x", 0)
+        .attr("y", function(d) { return rows[d.rank].middle + name_offset_y; })
+        // dominant-baseline=hanging puts the top of the text at the y-coord.
+        //.attr("dominant-baseline", "hanging")
+        .text(function(d) { return d.name + ": " + d.score; })
+    ;
+
+    // Place a rectangle behind the names to give them some contrast when
+    // they overlap bars. This must be defined after the names, because it
+    // uses the names variable. However, the background rect element must be
+    // defined before the the name text element in the svg, because
+    // otherwise the background would be displayed on top of the name. Use
+    // the d3.insert function to insert the background rect elements before
+    // the name text elements. For the d3.insert function to work, we need
+    // the name and its background to be defined within a group (which is
+    // probably a good idea anyway).
+
+    var name_bgs = svg.selectAll(".name_group")
+        .insert("rect", ".name")
+        .classed("name_bg", true)
+        .attr("x", function(d, i) { return names[0][i].getBBox().x - 2; })
+        .attr("y", function(d, i) { return names[0][i].getBBox().y - 2; })
+        .attr("width", function(d, i) { return names[0][i].getBBox().width + 4; })
+        .attr("height", function(d, i) { return names[0][i].getBBox().height + 4; })
+    ;
 }
 
 
